a033dfa84679ca159813c94d5a933c6993ac346e
nkxxll <niki.nice1203@gmail.com>
2024-12-06 17:41:44 +0100

    feat(day4): finished day4

--- day4/day4.ts
+++ day4/day4.ts
@@ -1,5 +1,6 @@
 import { parseArgs } from "util";
-const XMAS = /(XMAS|SAMX)/g;
+const XMAS = /XMAS/g;
+const SAMX = /SAMX/g;
 
 const { values } = parseArgs({
   args: Bun.argv,
@@ -36,12 +37,9 @@ function part1(text: string) {
   let res = 0;
   const split = text.split("\n");
   const horizon = [];
-  for (let i = 0; i < split.length; i++) {
-    const line = split[i];
-    const matches = line.match(XMAS);
-    res += (matches || []).length;
-  }
-
+  const diagonalsTopleftDownright = [];
+  const diagonalsToprightDownleft = [];
+  console.assert(split.length === split[0].length);
   for (let i = 0; i < split.length; i++) {
     for (let j = 0; j < split[i].length; j++) {
       if (i === 0) {
@@ -52,11 +50,32 @@ function part1(text: string) {
     }
   }
 
-  for (let i = 0; i < horizon.length; i++) {
-    const line = horizon[i];
-    const matches = line.match(XMAS);
-    res += (matches || []).length;
+  // diagonals
+  for (let i = 0; i < split.length; i++) {
+    for (let j = 0; j < split[0].length; j++) {
+      if (diagonalsTopleftDownright.length <= i + j) {
+        diagonalsTopleftDownright.push(split[i][j]);
+        continue;
+      }
+      diagonalsTopleftDownright[i + j] += split[i][j];
+    }
   }
+
+  // diagonals
+  for (let i = 0; i < split.length; i++) {
+    for (let j = 0; j < split[0].length; j++) {
+      if (diagonalsToprightDownleft.length <= i + j) {
+        diagonalsToprightDownleft.push(split[i][split[0].length - j - 1]);
+        continue;
+      }
+      diagonalsToprightDownleft[i + j] += split[i][split[0].length - j - 1];
+    }
+  }
+
+  res += getCount(split);
+  res += getCount(horizon);
+  res += getCount(diagonalsTopleftDownright);
+  res += getCount(diagonalsToprightDownleft);
   console.log(res);
 }
 function part2(text: string) {
