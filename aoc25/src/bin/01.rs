ef55c596ef85f2587deb0a40d86c3afdbafd7b3f
nkxxll <niki.nice1203@gmail.com>
2025-12-06 21:12:40 +0100

    day 1 part 1 rust

--- aoc25/src/bin/01.rs
+++ aoc25/src/bin/01.rs
@@ -5,22 +5,52 @@ use std::io::{BufRead, BufReader};
 const DAY: &str = "01"; // TODO: Fill the day
 const INPUT_FILE: &str = "input/01.txt";
 
-const TEST: &str = "
-<TEST-INPUT>
-"; // TODO: Add the test input
+const TEST: &str = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
 
 fn main() -> Result<()> {
-    //region Part 1
-    println!("=== Part 1 ===");
+    println!("=== Day {DAY} Part 1 ===");
 
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
-        // TODO: Solve Part 1 of the puzzle
-        let answer = reader.lines().flatten().count();
-        Ok(answer)
+        let mut acc = 50;
+        let mut sum = 0;
+
+        for line in reader.lines().flatten() {
+            let dir = line.as_bytes()[0] as char;
+            let n: i32 = line[1..].parse().expect("Failed to parse number");
+
+            match dir {
+                'L' => {
+                    acc -= n;
+                    while acc < 0 {
+                        acc += 100;
+                    }
+                }
+                'R' => {
+                    acc += n;
+                    while acc > 99 {
+                        acc -= 100;
+                    }
+                }
+                _ => panic!("Invalid direction"),
+            }
+            if acc == 0 {
+                sum += 1;
+            }
+        }
+        Ok(sum)
     }
 
     // TODO: Set the expected answer for the test input
-    assert_eq!(0, part1(BufReader::new(TEST.as_bytes()))?);
+    assert_eq!(3, part1(BufReader::new(TEST.as_bytes()))?);
 
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let result = part1(input_file)?;
